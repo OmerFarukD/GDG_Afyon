357365b38818267fe166c9743bbed0c26ce575e9
OmerFarukD <89133026+OmerFarukD@users.noreply.github.com>
2024-09-01 20:45:26 +0300

    Add project files.

---
+++ FirstDay/Program.cs
@@ -0,0 +1,184 @@
+﻿
+// Ö Ü ü ö İ ı Ğ Değişken ismi tanımlanırken kullanılmayacak kelimeler.
+// Değişken ismi tanımlanırken eğer rakamsal ifade kullanılacaksa değişken ismi rakam ile başlamamalı.
+// . ! ? - * / Değişken tanımlamsı yapılırken kullanılmaycak semboller. 
+// değişken ismi tanımlanırken küçük harfle başlar küçük harfle biter Eğer 2. kelime ihtiyacı olursa 2. Kelimenin ilk harfi
+// Büyük sonraki harfleri tekrar küçük olur.
+
+// veri_tipi değişken_adı = değişkenin_değeri;
+// string : Metinsel ifadeleri tutan veri tipidir.
+
+string metin = "C# Çok güzel bir programlama dilidir.";
+
+Console.WriteLine(metin);
+Console.WriteLine(metin);
+Console.WriteLine(metin);
+Console.WriteLine(metin);
+
+// int : Tam sayıları tutan bir veri tipidir.
+
+
+
+int sayi1 = 697;
+int sayi2 = 10;
+
+int toplam = sayi1 + sayi2;
+int cikarma = sayi1 - sayi2;
+int carpma = sayi1 * sayi2;
+int bolme = sayi1 / sayi2;
+
+
+
+Console.WriteLine(toplam);
+Console.WriteLine(cikarma);
+Console.WriteLine(carpma);
+Console.WriteLine(bolme);
+
+
+// double : Ondalıklı sayılar üzerinde daha hassas hesaplama yapar.
+Console.WriteLine("Double veri tipi ile alakalı işlemler aşağı da gösterilmektedir.");
+double number1= 654.25;
+double number2 = 125.78;
+
+double sum = number1 + number2;
+double difference = number1 - number2;
+double multiply  = number1 * number2;
+double divide = number1 / number2;
+
+Console.WriteLine(sum);
+Console.WriteLine(difference);
+Console.WriteLine(multiply);
+Console.WriteLine(divide);
+
+// bool : True , False Doğruluk değerlerini tuttuğumuz veri tipidir.
+bool dogru = true;
+Console.WriteLine(dogru);
+
+bool yanlis = false;
+Console.WriteLine(yanlis);
+
+// Operatörler
+
+// Aritemtik operatörler (+, -,*,/, % )
+
+// % : bir sayının diğerinden kalanını bulmak için kullanılır. 10 ile 3 ün bölümünden kalan 1
+Console.WriteLine(10%3);
+
+// Karşılaştırma Operatörleri
+
+// < küçüktür operatörü
+// > büyüktür operatörü
+// == eşit midir 
+// != eşit değil midir ? 
+// <= küçük eşit midir ?
+// >= büyük eşit midir ? 
+Console.WriteLine("Karşılaştırma Operatörleri.");
+
+Console.WriteLine(3<5); // True
+Console.WriteLine(5>3); // True
+Console.WriteLine(15==15); //True
+Console.WriteLine(25!=25); // False 
+Console.WriteLine(23<=23); // True
+Console.WriteLine(78>=56); // True
+
+// Mantıksal Operatörler 
+// ve (&&) : Her iki koşul True ise True döner aksi durumlarda ise False cevabını verir.
+// veya (||): Koşullardan herhangi biri True ise True değerini döner.
+// Değil (!) : Koşulun sonucunun tersini alır
+
+Console.WriteLine("Mantıksal Operatörler.");
+
+bool sonuc = 3 < 5 && 5 > 9;
+//              True   ve  False  = False
+
+bool sonuc1 = 3 < 5 || 5 > 9;
+//             True  veya False = True
+Console.WriteLine(sonuc);
+Console.WriteLine(sonuc1);
+
+Console.WriteLine(!sonuc);
+Console.WriteLine(!sonuc1);
+
+// Atama Operatörleri : (=, +=, *=,/=, %=)
+
+int a = 40;
+
+
+// Toplam atama 
+//1. Yöntem 
+//a = a + 5;
+
+// 2. Yöntem
+//a += 5;
+
+
+// Çarpım atama 
+//a = a * 5;
+
+a *= 5;
+
+// Bölüm atama 
+//a = a / 10;
+//a /= 10;
+
+//a = a % 20;
+a %= 20;
+Console.WriteLine(a);
+
+// Arttırma ve azaltma operatörleri  (++,--)
+
+Console.WriteLine("Arttırma ve azaltma operatörleri.");
+int b = 10;
+
+
+// Prefix increment 
+Console.WriteLine(++b);
+
+int x = 25;
+Console.WriteLine(--x);
+
+// Postfix increment 
+Console.WriteLine(x++);
+
+// Prefix increment  Örneği
+Console.WriteLine("Prefix increment");
+int m = 5;
+int n = ++m;
+
+Console.WriteLine(m); //6
+Console.WriteLine(n); // 6
+
+
+// Postfix increment  Örneği
+Console.WriteLine("Postfix increment");
+
+int j = 5;
+int k = j++; 
+Console.WriteLine(j);
+Console.WriteLine(k);
+
+string ad = "Ömer Faruk ";
+Console.WriteLine(ad+="Doğan");
+
+// Ternary Koşul operatörü(?);
+
+// bir tane yaş değişkeni tanımlayınız ve bu yaş değişkeninin değeri 18 den büyük eşit ise 
+// Ekran çıktısı olarak Ehliyet alabilir yazısı çıksın aksi durumda ise Ehliyet alamaz
+
+int yas = 10;
+string ehliyet = yas >= 18 ? "Eyliyet alabilir" : "Ehliyet alamaz.";
+Console.WriteLine(ehliyet);
+
+int number3 = 25;
+int number4 = 25;
+
+
+int age = 38;
+string name = "Mehmet";
+string surname = "Eroğlu";
+
+string cikti = "Merhaba benim adım " + name + ", Soyadım " + surname + " Yaşım " + age;
+Console.WriteLine(cikti);
+
+string buyuktur = number3 > number4 || number3==number4  ? number3 + " " + number4 + " den büyüktür"  : number4 + " " + number3 + "den büyüktür";
+Console.WriteLine(buyuktur);
---
+++ KararMekanizmalari/Program.cs
@@ -0,0 +1,172 @@
+﻿// if else  
+// kullanıcıdan konsol yardımı ile bir yaş alınız eğer girrmiş olduğu yaş değeri
+//18 den büyük eşit ise Ehliyet alabilir aksi durumda ehliyet alamaz
+
+//Console.WriteLine("Lütfen Yaşınızı Giriniz : ");
+//int yas =Convert.ToInt32(Console.ReadLine());
+//1. Yöntem ternary operatörü(?)
+//string sonuc = yas >= 18 ? "Ehliyet alabilir" : "Ehliyet alamaz.";
+//Console.WriteLine(sonuc);
+// Çoklu satırları yorum satırına alan kısayol : Ctrl + k + C
+// 2. Yöntem if else 
+
+//if (yas>=18)
+//{
+//    Console.WriteLine("Ehliyet alabilir...");
+//}
+//else
+//{
+//    Console.WriteLine("Ehliyet Alamaz...");
+//}
+
+
+//görev : Kullanıcıdan age değişken adlı integer değer alan konsoldan bir girdi alınız.
+// age < 13 Çocuk 
+// age <18 Ergen
+// age< 55 yetişkin aksi durumda ie Yaşlı 
+// else if
+//Console.WriteLine("Lütfen Bir yaş değeri giriniz. ");
+//int age = Convert.ToInt32(Console.ReadLine());
+
+//if (age<13)
+//{
+//    Console.WriteLine("Çocuk");
+//}
+
+//else if (age < 18)
+//{
+//    Console.WriteLine("Ergen");
+//}
+
+
+//else if ( age < 55)
+//{
+//    Console.WriteLine("Yetişkin.");
+//}
+//else
+//{
+//    Console.WriteLine("Yaşlı");
+//}
+
+
+// Kullanıcının sisteme giriş yapmasını istiyoruz bilgiler doğruysa
+// giriş yapıldı.
+// Aksi durumda giriş yapılırken hangi alan hatalıysa o alanla alakalı bilgi verilsin.
+
+
+//string username = "admin";
+//string password = "12345";
+
+//Console.WriteLine("Lütfen kullanıcı adınızı giriniz : ");
+//string usernameInput = Console.ReadLine();
+
+//Console.WriteLine("Lütfen parolanızı giriniz: ");
+//string passwordInput = Console.ReadLine();
+
+
+//if (username !=usernameInput || password != passwordInput)
+//{
+//    Console.WriteLine("Lütfen bilgilerinizi kontrol ediniz.");
+//    Console.WriteLine("Parola veya kullanıcı adı hatalıdır.");
+//}
+
+//else
+//{
+//    Console.WriteLine("Giriş başarılı.");
+//}
+
+// Bir e ticaret sitesinden seçmiş olduğu üründen kaç tane aldığını ve 
+// yapmış olduğu alışveriş sonucunda ne kadar indirim uygulanacığını hesaplayan bir sistem 
+
+// Kullanıcının  bütçesi : 10000
+// Pantolon : 1500
+// gömlek : 2500
+// Takım Elbise 1 : 5000
+// Takım Elbise 2 : 10000
+// Saat : 3500
+
+// kullanıcıdan kaç adet almak istediğini sor
+// adet sayısı * ürün = total ücret
+// total ücret = 5000 den fazla ise total ücrete %15 indirim yap
+// total ücret = 7000 den fazla ise total ücrete %20 indirim yap
+// total ücret = 8000 den fazla ise total ücrete %25 indirim yap
+// kullanıcının bütçesi 10000 i aşarsa ekran yazısı olarak Yetersiz Bakiy yazsın
+
+double budget = 10000;
+double pant = 1500;
+double shirt = 2500;
+double suit1 = 5000;
+double suit2 = 10000;
+double watch = 3500;
+
+Console.WriteLine("Pantolon fiyatı: "+pant);
+Console.WriteLine("Gömlek fiyatı: " + shirt);
+Console.WriteLine("Takım elbise 1 fiyatı: " + suit1);
+Console.WriteLine("Takım elbise 2 fiyatı: " + suit2);
+Console.WriteLine("Saat fiyatı: " + watch);
+
+Console.WriteLine("Kaç adet pantolon almak istersiniz ?");
+int countPant = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Kaç adet gömlek almak istersiniz ?");
+int shirtCount = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Kaç adet Takım elbise 1 almak istersiniz ?");
+int countSuit1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Kaç adet Takım elbise 2 almak istersiniz ?");
+int countSuit2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Kaç adet Saat almak istersiniz ?");
+int countWatch = Convert.ToInt32(Console.ReadLine());
+
+// (pantolon_ücreti* pantolon adet) + (gömlek_ücreti* gömlek adet)
+
+double pantTotal = pant * countPant;
+double shirtTotal = shirt * shirtCount;
+double suitTotal = suit1 * countSuit1;
+double suit2Total = suit2 * countSuit2;
+double watchTotal = watch * countWatch;
+
+double totalPrice = pantTotal + shirtTotal+ suitTotal + suit2Total+watchTotal;
+
+if (totalPrice > 10000)
+{
+    Console.WriteLine("Yetersiz bakiye.");
+    return;
+}
+
+
+if (totalPrice > 5000)
+{
+    double indirimsiz = totalPrice;
+    Console.WriteLine("İndirimsiz fiyatı : " + indirimsiz);
+    totalPrice = totalPrice - totalPrice * 0.15;
+    Console.WriteLine("İndirimli fiyatı: "+totalPrice);
+
+    double fark = indirimsiz - totalPrice;
+    Console.WriteLine("Aradaki ücret farkı :" + fark);
+}
+ else if (totalPrice > 7000)
+{
+    double indirimsiz = totalPrice;
+    Console.WriteLine("İndirimsiz fiyatı : " + indirimsiz);
+  
+    totalPrice = totalPrice - totalPrice * 0.20;
+    Console.WriteLine("İndirimli fiyatı: " + totalPrice);
+
+    double fark = indirimsiz - totalPrice;
+    Console.WriteLine("Aradaki ücret farkı :" + fark);
+}
+
+else if (totalPrice > 8000)
+{
+    double indirimsiz = totalPrice;
+    Console.WriteLine("İndirimsiz fiyatı : "+indirimsiz);
+    
+    totalPrice = totalPrice - totalPrice * 0.25;
+    Console.WriteLine("İndirimli fiyatı: " + totalPrice);
+
+    double fark = indirimsiz - totalPrice;
+    Console.WriteLine("Aradaki ücret farkı :"+fark);
+}
